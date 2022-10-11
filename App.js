a407b813f21b1e8207d352b78ce7749c19565ce7
Luke Runnels <lar9482@utulsa.edu>
2022-10-11 13:05:02 -0500

    Initial test for DB in App.js

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
-"use strict";
-exports.__esModule = true;
-var expo_status_bar_1 = require("expo-status-bar");
-var react_native_1 = require("react-native");
-function App() {
-    return (<react_native_1.View style={styles.container}>
-      <react_native_1.Text>Start of TU APP. This is our senior project for 2022-2023.</react_native_1.Text>
-      <expo_status_bar_1.StatusBar style="auto"/>
-    </react_native_1.View>);
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as SQLite from 'expo-sqlite'
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+
+
+var databasePath = 'C:\\Users\\luker\\source\\repos\\Start-TU-App\\Start-TU-App\\Database';
+var databaseName = 'TU_Database.db';
+
+async function openDatabase() {
+  if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+    await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+  }
+  await FileSystem.downloadAsync(
+    Asset.fromModule(require(databasePath + '\\' + databaseName)).uri,
+    FileSystem.documentDirectory + 'SQLite/' + databaseName
+  );
+  return SQLite.openDatabase(databaseName);
 }
-exports["default"] = App;
-var styles = react_native_1.StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
+
+export default function App() {
+  openDatabase().then(
+    db => {
+      db.transaction((tx)=> {
+        tx.executeSql(
+          "CREATE TABLE IF NOT EXISTS Person"
+          + "(" 
+          + "TU_Email NVARCHAR(18) PRIMARY KEY NOT NULL"
+          + ", Name TEXT"
+          + ", Password TEXT"
+          + ");"
+      );
+      })
     }
+  ); 
+
+  console.log(databasePath);
+  console.log(databaseName);
+
+  return (
+    <View style={styles.container}>
+      <Text>Start of TU APP. This is our senior project for 2022-2023.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
