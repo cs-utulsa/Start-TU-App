36f2a0751fa0c552822b3f9cece71d9a8c44896c
Ben Hughes <hughesbenm@gmail.com>
2022-10-30 20:14:02 -0500

    Fixes weird merge issues

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -11,18 +10,6 @@ function HomeScreen() {
       <Text>Home!</Text>
     </View>
   );
-=======
-"use strict";
-exports.__esModule = true;
-var expo_status_bar_1 = require("expo-status-bar");
-var react_native_1 = require("react-native");
-
-function App() {
-    return (<react_native_1.View style={styles.container}>
-      <react_native_1.Text>Start of TU APP. This is our senior project for 2022-2023.</react_native_1.Text>
-      <expo_status_bar_1.StatusBar style="auto"/>
-    </react_native_1.View>);
->>>>>>> Stashed changes
 }
 
 function SettingsScreen() {
--- App.tsx
+++ App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-=======
 import React, { useState } from 'react';
 import { Text, View, StyleSheet, Button, Alert, Image, TouchableOpacity, Switch } from 'react-native';
 import {StatusBar} from 'expo-status-bar';
@@ -11,38 +7,26 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const TU_GOLD = '#C2A01E'
 const TU_BLUE = '#102240'
-var mapImage = [require('./assets/TUmap.png'), require('./assets/TUmapinv.png')];
 var mapImageType = 0
-const images = {
-  mainButtons: {
-    map: require('./assets/TUmap.png'),
-  },
-};
+// const images = {
+//   mainButtons: {
+//     // map: require('./assets/TUmap.png'),
+//   },
+// };
 const invert = () => mapImageType = 1;
 
->>>>>>> Stashed changes
 
 export default function App() {
   return (
-<<<<<<< Updated upstream
-    <View style={styles.container}>
-      <Text>Start of TU APP. This is our senior project for 2022-2023.</Text>
-      <StatusBar style="auto" />
-=======
     <View style={{flex: 1, backgroundColor: TU_BLUE}}>
       <MapView style={{height: '50%', width: '100%'}}/> 
       <View style={{paddingTop:20, padding: 5}}>
-        <Image
-          style={{width: 50, height: 50}}
-          source={require('./assets/TUlogo.png')}
-        />
+        <Text> Icon </Text>
       </View>
       <View style={{flex: 1}}>
         <StatusBar style="light"/>
-        <ClassesPane></ClassesPane>
-        <BottomButtons onPress={changeImage} invImage={invImage}></BottomButtons>
+        <Text> Hello</Text>
       </View>
->>>>>>> Stashed changes
     </View>
   );
 }
