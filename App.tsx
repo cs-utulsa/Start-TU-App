4baca2412031d96b444336debf4b4f36632a256c
Luke Runnels <lar9482@utulsa.edu>
2022-11-01 12:59:27 -0500

    Test cases for the query buttons

--- App.tsx
+++ App.tsx
@@ -6,14 +6,42 @@ import {Person, Person_Data} from './Database/Person';
 import {Location, Location_Data} from './Database/Location';
 
 export default function App() {
-  return (
+
+    const person1: Person_Data = {
+      TU_Email: "lar9482@utulsa.edu",
+      Name: "Luke Runnels",
+      Password: "password123"
+    };
+
+    const person2: Person_Data = {
+      TU_Email: "jdd9482@utulsa.edu",
+      Name: "John Doe",
+      Password: "password456"
+    };
+
+    const location1: Location_Data = {
+      Name: "Keplinger Hall",
+      Address: "TU Campus",
+      Latitude: 100,
+      Longitude: 200
+    };
+
+    Person.createPersonTable();
+    Location.createLocationTable();
+
+    Person.insertIntoPersonTable(person1);
+    Person.insertIntoPersonTable(person2);
+    
+    Location.insertIntoLocationTable(location1);
+
+    return(
     <View style={styles.container}>
       <Text>Database Testing Branch</Text>
       <StatusBar style="auto" />
       <QueryPerson queryFunction={Person.queryAllAttributes_Async()} title ={"Query Person"}></QueryPerson>
       <QueryLocation queryFunction={Location.queryAllAttributes_Async()} title = {"Query Location"}></QueryLocation>
     </View>
-  );
+    );
 }
 
 const QueryPerson = ({queryFunction, title}: any) => (
