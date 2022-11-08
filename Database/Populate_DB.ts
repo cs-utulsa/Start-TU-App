7234dfdc7046e1a278ff03e8c55d99751b2c0da2
Luke Runnels <lar9482@utulsa.edu>
2022-11-08 00:47:16 -0600

    Added some marker locations to the database

--- Database/Populate_DB.ts
+++ Database/Populate_DB.ts
@@ -47,6 +47,54 @@ const John_Mabee: Location_Data = {
     Tags: ["all", "housing"]
 };
 
+const Lottie_John_Mabee: Location_Data = {
+    Name: "Lottie John Mabee",
+    Description: "All Females' Dorm",
+    Latitude: 36.15133476567233,
+    Longitude: -95.94871428765713,
+    Tags: ["all", "housing"]
+};
+
+const Philips_Hall: Location_Data = {
+    Name: "Philips Hall",
+    Description: "School of Art",
+    Latitude: 36.15299141037871,
+    Longitude: -95.94642195707841,
+    Tags: ["all", "a&s"]
+};
+
+const Helmerich_Hall: Location_Data = {
+    Name: "Helmerich Hall",
+    Description: "Building of the Collins College of Business",
+    Latitude: 36.15324781305708,
+    Longitude: -95.94717706214203,
+    Tags: ["all", "a&s"]
+};
+
+const Chapman_Hall: Location_Data = {
+    Name: "Chapman Hall",
+    Description: "Building of the Kendall College of Arts & Science",
+    Latitude: 36.15308684418934,
+    Longitude: -95.94790750389673,
+    Tags: ["all", "a&s"]
+};
+
+const Lorton_Hall: Location_Data = {
+    Name: "Lorton Hall",
+    Description: "Building of the Department of Psychology",
+    Latitude: 36.15142254421786,
+    Longitude: -95.94796814388067,
+    Tags: ["all", "a&s"]
+};
+
+const Mcfarlin_Library: Location_Data = {
+    Name: "Mcfarlin Library",
+    Description: "Main Academic Library",
+    Latitude: 36.15232374393028,
+    Longitude: -95.94599221560202,
+    Tags: ["all", "Library"]
+};
+
 export function populate() {
     Person.createPersonTable();
     Location.createLocationTable();
@@ -57,5 +105,11 @@ export function populate() {
     Location.insertIntoLocationTable(Rayzor_Hall);
     Location.insertIntoLocationTable(Stephenson_Hall);
     Location.insertIntoLocationTable(John_Mabee);
+    Location.insertIntoLocationTable(Lottie_John_Mabee);
     Location.insertIntoLocationTable(Student_Union);
+    Location.insertIntoLocationTable(Philips_Hall);
+    Location.insertIntoLocationTable(Helmerich_Hall);
+    Location.insertIntoLocationTable(Chapman_Hall);
+    Location.insertIntoLocationTable(Lorton_Hall);
+    Location.insertIntoLocationTable(Mcfarlin_Library);
 }
