70081cfa8641fef5f779cc3b6213a92c7c9f56e1
Luke Runnels <lar9482@utulsa.edu>
2022-12-01 01:07:36 -0600

    Added state to direction button's position to prevent jumping when the modal opens.

--- PaneComponents/MapPaneComponents/RoutingPopup.tsx
+++ PaneComponents/MapPaneComponents/RoutingPopup.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, FC } from 'react';
-import {StyleSheet, TouchableOpacity, Image, Modal, Text, View, TextInput} from 'react-native'
+import {StyleSheet, TouchableOpacity, Image, Modal, Text, View, TextInput, Button, ImageBackground} from 'react-native'
 import {Location, Location_Data} from '../../Database/Location';
 
 type RoutingPopupProps = {
@@ -9,32 +9,64 @@ type RoutingPopupProps = {
 
 export const RoutingPopup = () => {
     const [viewPopup, toggleViewPopup] = useState<boolean>(false)
+    const [tempOrigin, setTempOrigin] = useState<string>("Mcfarlin Library");
+    const [tempDestination, setTempDestination] = useState<string>("Keplinger Hall");
+
+    const [top, setTop] = useState<number>(10);
+    const [left, setLeft] = useState<number>(3);
 
     return(
         <TouchableOpacity disabled = {false} onPress = {() => {
           toggleViewPopup(!viewPopup);
         }} activeOpacity = {1} style = {styles.button} >
-          {!viewPopup && <Image style={styles.icon} source={require('../../assets/Directions.png')}></Image>}
+          <Image style={{position: 'absolute',
+            top: top,
+            left: left,
+            width: 54,
+            height: 55,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 5}} source={require('../../assets/Directions.png')}></Image>
+          
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={viewPopup}
+              onDismiss ={() => {
+                console.log(tempOrigin);
+                console.log(tempDestination);
+                console.log('Modal closed');
+              }}
+              onShow = {() => {setTop(-7.5); setLeft(-5)}}>
 
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={viewPopup}>
             
             <View style={styles.popupView}>
               <View style={styles.textInputViewTo}>
-                <TextInput placeholder="To:" style={styles.textInput}></TextInput>
+                <TextInput placeholder="To:" style={styles.textInput} onSubmitEditing = {(e) => {
+                  setTempOrigin(e.nativeEvent.text);
+                }}></TextInput>
               </View>
+
               <View style={styles.textInputViewFrom}>
-              <TextInput placeholder="From: " style={styles.textInput}></TextInput>
+                <TextInput placeholder="From: " style={styles.textInput} onSubmitEditing = {(e) => {
+                  setTempDestination(e.nativeEvent.text)
+                }}></TextInput>
               </View>
+
+              <Button title={"Close"} onPress = {() => {toggleViewPopup(!viewPopup)}}></Button>
             </View>
           </Modal>
-        </TouchableOpacity>
-    );
+          
+          </TouchableOpacity>
+    
+        
+    )
 }
 
 const styles = StyleSheet.create({
+    onPressRoute: {
+      backgroundColor: 'black'
+    },
     textInputViewTo: {
       position: "relative",
       alignContent: "flex-start",
@@ -56,8 +88,9 @@ const styles = StyleSheet.create({
       width: 195,
       backgroundColor: "#DADADA",
     },
+
     popupView: {
-      position: "relative",
+      position: "absolute",
       top: 380,
       justifyContent: "center",
       alignSelf: "center",
@@ -66,20 +99,21 @@ const styles = StyleSheet.create({
       backgroundColor: 'white',
       borderRadius: 20,
       borderColor: "black",
-      borderWidth: 2.5
+      borderWidth: 2.5,
+    
     },
 
     button: {
-      position: 'absolute',
-      top: 15,
-      left: 10,
-      width: 40,
-      height: 40,
+      position: 'relative',
+      top: 20,
+      left: 12,
+      width: 35,
+      height: 35,
       justifyContent: 'center',
       alignItems: 'center',
       padding: 5,
-      transform: [{ rotate: "45deg" }],
-      //backgroundColor: 'white'
+      //transform: [{ rotate: "45deg" }],
+      //backgroundColor: 'black'
     },
   
     icon:{
@@ -91,7 +125,7 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
       padding: 5,
-      //transform: [{ rotate: "45deg" }],
+      // transform: [{ rotate: "45deg" }],
       // borderRadius: 100,
       //backgroundColor: 'white'
     }
