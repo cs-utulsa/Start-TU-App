44b33c22932d712f359e7c119447fa0eafd07732
Luke Runnels <lar9482@utulsa.edu>
2022-12-29 16:41:35 -0600

    Added sql to create email table in the database.

--- Database/Email.ts
+++ Database/Email.ts
@@ -7,4 +7,30 @@ class Email_Entity {
     constructor(database_name: string) {
         this.DB = SQLite.openDatabase(database_name);
     }
+
+    createEmailTable() {
+        this.DB.transaction(
+            (tx) => {
+                const sqlCommand: string =
+                 "Create Table Email( " +
+                 "	Id varchar(250) PRIMARY KEY, " +
+                 "	Sender_Email varchar(50) NOT NULL, " +
+                 "  Folder varchar(25), " +
+                 "	Subject varchar(10000), " +
+                 "	Body varchar(10000000), " +
+                 "	ReceivedTime datetime, " +
+                 "  CONSTRAINT CHK_Folder CHECK(Folder='Inbox') " +
+                 ");";
+
+                 tx.executeSql(sqlCommand);
+            },
+            (error) => {
+                console.log(error.message);
+            },
+            () => {
+                console.log('Successfully created the email table');
+            }
+        );
+    }
 }
+
