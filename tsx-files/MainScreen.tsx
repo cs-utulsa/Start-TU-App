3160ee893bae59660df80bd000812d0d88141d75
Andres-galiasso <andres@galiasso.net>
2023-01-20 13:39:10 -0600

    Working?

---
+++ tsx-files/MainScreen.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button, Alert, Image, TouchableOpacity, Switch, TextInput } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import { getTokenSourceMapRange, isPropertySignature, setTextRange } from 'typescript';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+
+
+interface MainScreenInterface {
+    naviagtion: any;
+}
+
+const MainPage = (screenInterface: MainScreenInterface) => {
+
+    
+
+}
+
+
+export default MainPage;
