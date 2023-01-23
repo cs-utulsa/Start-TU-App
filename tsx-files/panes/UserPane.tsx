1379217d82cb8143692a17e23544457c188911af
Luke Runnels <lar9482@utulsa.edu>
2023-01-23 12:06:07 -0600

    Updated the UserPane component in tsx-files

--- tsx-files/panes/UserPane.tsx
+++ tsx-files/panes/UserPane.tsx
@@ -6,10 +6,19 @@ import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 import styles from './PaneStyles';
 
-const UserPane = () => (
+const UserPane = () => {
+  return(
     <View style={styles.userPane}>
-      <Text> USERS ARE NOT YET IMPLEMENTED</Text>
+        <Text style={{fontSize: 50}}> Ben</Text>
+        <Text style={{fontSize: 20}}> Benjamin Hughes </Text>
+        <Text style={{fontSize: 5}}> {"\n"} </Text>
+        <Text style={{fontSize: 20}}> bmh7113@utulsa.edu</Text>
+        <Text style={{fontSize: 10}}> {"\n"} </Text>
+        <Text style={{fontSize: 30}}> Bachelor of Science</Text>
+        <Text style={{fontSize: 20}}> Major in Computer Science</Text>
+        <Text style={{fontSize: 20}}> Minor in Spanish</Text>
     </View>
-  );
+  )
+}
 
-  export default UserPane;
+export default UserPane;
