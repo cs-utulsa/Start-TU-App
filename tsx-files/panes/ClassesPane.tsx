a73d70b66eac7ec85c886c3edfcae184fd2b1560
Luke Runnels <lar9482@utulsa.edu>
2023-01-23 11:58:46 -0600

    Updated the ClassesPane component in tsx-files

--- tsx-files/panes/ClassesPane.tsx
+++ tsx-files/panes/ClassesPane.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button, Alert, Image, TouchableOpacity, Switch, TextInput } from 'react-native';
-import {StatusBar} from 'expo-status-bar';
-import { getTokenSourceMapRange, isPropertySignature, setTextRange } from 'typescript';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import {View, Image} from 'react-native';
 
 import styles from './PaneStyles';
 
-const ClassesPane = () => (
-    <View style={styles.classesPane}>
-      <Text> CLASSES ARE NOT YET IMPLEMENTED</Text>
-    </View>
-  );
+const ClassesPane = () => {
+    return(
+      <View style={styles.classesPane}>
+        <Image style={{aspectRatio: 0.63, height: 595}} source={require('../assets/Harvey.png')} />
+      </View>
+    )
+}
 
-  export default ClassesPane;
+export default ClassesPane;
