695aa98889841905e024f60a4680be05655a3846
Ben Hughes <hughesbenm@gmail.com>
2023-01-26 01:16:25 -0600

    Final Building Before Demo

--- Panes/MapPane.tsx
+++ Panes/MapPane.tsx
@@ -100,10 +100,21 @@ const MapPane= () => {
                 coordinate={{latitude: item.Latitude, longitude: item.Longitude}}
                 title={item.Name}
                 description={item.Description}
-                image={require('./../assets/Location-Marker.png')} style={{height: 5, width:5 }}
               >
+                <Image
+                  source={require('./../assets/Location-Marker.png')}
+                  style={{width: 26, height: 28}}
+                />
               </Marker>
             ))}
+            <Marker
+              coordinate={{latitude: 36.153627613433635, longitude: -95.94216178640234}}
+            >
+              <Image
+                source={require('./../assets/EventPinBlueLow.png')}
+                style={{width: 100, height: 50}}
+              />
+            </Marker>
       </MapView> 
     </View>
   );
