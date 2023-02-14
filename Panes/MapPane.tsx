d7904c789c6363bf16de7738e3764c6d54122896
Ben Hughes <hughesbenm@gmail.com>
2023-02-14 12:24:49 -0600

    Adds some comments

--- Panes/MapPane.tsx
+++ Panes/MapPane.tsx
@@ -23,24 +23,19 @@ const MapPane= () => {
     {} as Location_Data
   );
 
+  //State for the tag system
   const [currentTag, setCurrentTag] = useState<string>("all");
 
-  
+  //Not sure what this does
   useEffect(() => {
     Location.queryAttributes_Tag(currentTag).then((value:Location_Data[]) => {
       setMarkerData(value);
     });
   }, [currentTag]);
   
-  const updateDirectionEndpoints = (newOrigin: Location_Data, newDestination: Location_Data) => {
-    setOrigin(newOrigin);
-    setDestination(newDestination);
-  };
-
-  
   return(
-    <View style={styles.mapPane}>
-      <View style={{padding:5, paddingBottom:10, height: 50}}>
+    <View style={styles.mapPane}> {/* Start main view */}
+      <View style={{padding:5, paddingBottom:10, height: 50}}> {/* Start search bar view */}
         <TextInput onSubmitEditing={
           (e) => {
             const tagInput: string = e.nativeEvent.text.toLowerCase();
@@ -52,69 +47,89 @@ const MapPane= () => {
       </View>
 
       <MapView 
-          showsBuildings={false}
-          showsPointsOfInterest={false}
-          initialRegion={{
-            latitude: 36.15236,
-            longitude: -95.94575,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.0125,}} 
-          style = {{height: '100%', width: '100%'}}
-          onLongPress = {(e) => {
-            const location: LatLng = e.nativeEvent.coordinate
-            setOrigin(destination)
-            setDestination({
-              Latitude: location.latitude, 
-              Longitude: location.longitude
-            } as Location_Data)
-          }}>
+        showsBuildings={false}
+        showsPointsOfInterest={false}
+        initialRegion={{
+          latitude: 36.15236,
+          longitude: -95.94575,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.0125,}} 
+        style = {{height: '100%', width: '100%'}}
+        onLongPress = {(e) => {
+          const location: LatLng = e.nativeEvent.coordinate
+          setOrigin(destination)
+          setDestination({
+            Latitude: location.latitude, 
+            Longitude: location.longitude
+          } as Location_Data)
+        }
+      }>{/* Start MapView view */}
 
-            <BuildingList></BuildingList>
+      <BuildingList></BuildingList>
 
-            { 
-              JSON.stringify(origin) != '{}' && 
-              JSON.stringify(destination) != '{}' &&
+      {/* Check for both endpoints of directions 
+          If both are there, display directions */}
+      { JSON.stringify(origin) != '{}' && 
+        JSON.stringify(destination) != '{}' &&
 
-              <MapViewDirections
-              origin={{latitude: origin.Latitude, longitude: origin.Longitude}}
-              destination={{latitude: destination.Latitude, longitude: destination.Longitude}}
-              apikey={GOOGLE_MAPS_API_KEY}
-              mode={"WALKING"}
-              strokeColor={TU_LIGHT_BLUE}
-              strokeWidth={3}/>
-            }
+        <MapViewDirections
+          origin={{latitude: origin.Latitude, longitude: origin.Longitude}}
+          destination={{latitude: destination.Latitude, longitude: destination.Longitude}}
+          apikey={GOOGLE_MAPS_API_KEY}
+          mode={"WALKING"}
+          strokeColor={TU_LIGHT_BLUE}
+          strokeWidth={3}
+        />
+      }
 
-            {
-              JSON.stringify(origin) != '{}' &&
-              <Marker coordinate={{latitude: origin.Latitude, longitude: origin.Longitude}} pinColor={'blue'}></Marker>
-            }
-              
-            {
-              JSON.stringify(destination) != '{}' &&
-              <Marker coordinate={{latitude: destination.Latitude, longitude: destination.Longitude}} pinColor={'green'}></Marker>
-            }
-          
-            {markerData.map((item: Location_Data, index:number) => (
-              <Marker
-                key={index}
-                coordinate={{latitude: item.Latitude, longitude: item.Longitude}}
-                title={item.Name}
-                description={item.Description}
-              >
-                <Image
-                  source={require('./../assets/Location-Marker.png')}
-                  style={{width: 26, height: 28}}
-                />
-              </Marker>
-            ))}
-            <Marker
-              coordinate={{latitude: 36.153627613433635, longitude: -95.94216178640234}}
-            >
-              <Image
-                source={require('./../assets/EventPinBlueLow.png')}
-                style={{width: 100, height: 50}}
-              />
-            </Marker>
+      {/* Check for origin and display */}
+      { JSON.stringify(origin) != '{}' &&
+
+        <Marker coordinate={{latitude: origin.Latitude, longitude: origin.Longitude}} pinColor={'blue'}></Marker>
+      }
+      
+      {/* Check for destination and display */}
+      { JSON.stringify(destination) != '{}' &&
+
+        <Marker coordinate={{latitude: destination.Latitude, longitude: destination.Longitude}} pinColor={'green'}></Marker>
+      }
+    
+      { markerData.map((item: Location_Data, index:number) => (
+        <Marker
+          key={index}
+          coordinate={{latitude: item.Latitude, longitude: item.Longitude}}
+          title={item.Name}
+          description={item.Description}
+        >
+          <Image
+            source={require('./../assets/Location-Marker.png')}
+            style={{width: 26, height: 28}}
+          />
+        </Marker>
+      ))}
+      
+      { markerData.map((item: Location_Data, index:number) => (
+        <Marker
+          key={index}
+          coordinate={{latitude: item.Latitude, longitude: item.Longitude}}
+          title={item.Name}
+          description={item.Description}
+        >
+          <Image
+            source={require('./../assets/Location-Marker.png')}
+            style={{width: 26, height: 28}}
+          />
+        </Marker>
+      ))}
+
+      <Marker
+        coordinate={{latitude: 36.153627613433635, longitude: -95.94216178640234}}
+      >
+        <Image
+            source={require('./../assets/EventPinBlueLow.png')}
+            style={{width: 100, height: 50}}
+          />
+        </Marker>
       </MapView> 
     </View>
   );
