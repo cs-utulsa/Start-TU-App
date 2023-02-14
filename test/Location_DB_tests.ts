5e3eac955ea62aef5ddd047eef88f8525f9099eb
Luke Runnels <lar9482@utulsa.edu>
2023-02-14 13:06:59 -0600

    Beginning unit tests for the location table

---
+++ test/Location_DB_tests.ts
@@ -0,0 +1,20 @@
+import {Location, Location_Entity} from '../Database/Location'
+
+const db_name: string = 'Test_DB.db';
+const Location_Test: Location_Entity = new Location_Entity(db_name)
+
+function createTest() {
+    console.log('Beginning create table test for location')
+}
+
+function insertTest() {
+
+}
+
+function queryTest_Valid() {
+
+}
+
+function queryTest_Invalid() {
+
+}
