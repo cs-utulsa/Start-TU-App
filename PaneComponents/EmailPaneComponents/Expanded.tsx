c66373ded199d2709c8dab9312318f0bbbf6b4d5
Luke Runnels <lar9482@utulsa.edu>
2023-03-28 12:59:40 -0500

    Wrote basic expanded email mockup

--- PaneComponents/EmailPaneComponents/Expanded.tsx
+++ PaneComponents/EmailPaneComponents/Expanded.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, FC } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, ScrollView} from 'react-native';
 import { Email_Data } from '../../Database/Email';
 import { Modal, Pressable, Button } from 'react-native';
 
@@ -19,7 +19,16 @@ export const Expanded: FC<ExpandedProps> = ({item, visible, setVisible}) => {
                     setVisible(!visible)
                 }}></Button>
             </View>
+            
+            <View style = {styles.subjectContainer}>
+                <Text style={{fontSize: 40}}>{item.Subject}</Text>
+            </View>
 
+            <ScrollView style = {styles.bodyContainer}>
+                <Text style={{fontSize: 20}}>{item.Sender_Email}</Text>
+                {<Text style={styles.bodyContainerHeader}>{item.receivedDateTime.toString()}</Text>}
+                <Text style={{fontSize: 20}}>{item.Body}</Text>
+            </ScrollView>
         </View>
     )
 }
@@ -34,9 +43,28 @@ const styles = StyleSheet.create({
     },
 
     buttonContainer: {
-        // backgroundColor: 'red',
+        //backgroundColor: 'red',
         width: '16%',
         height: '5%',
-        left: 5
+        left: 5,
+    },
+
+    subjectContainer: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: '#F1F0F0',
+        borderBottomWidth: 1,
+        borderBottomColor: '#C9C8C8'
+    },
+
+    bodyContainer: {
+        width: '100%',
+        height: '85%',
+        backgroundColor: '#F1F0F0'
+    },
+
+    bodyContainerHeader: {
+        paddingBottom: 30,
+        fontSize: 20
     }
 });
