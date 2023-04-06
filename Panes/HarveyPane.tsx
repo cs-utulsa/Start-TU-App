21c1931f60d4d739306950f21a2670a86dcc8b12
Jason Alfrey <jsa0448@utulsa.edu>
2023-04-06 12:54:15 -0500

    Adjusted Harvey Class Rendering

--- Panes/HarveyPane.tsx
+++ Panes/HarveyPane.tsx
@@ -3,10 +3,6 @@ import {StyleSheet, View, Text} from 'react-native';
 import { useWindowDimensions } from 'react-native';
 import RenderHtml from 'react-native-render-html';
 
-const jsonSource = {
-  html: "<p style='text-align:center;'>Placeholder for json data</p>"
-};
-
 export default function List() {
   const harveyList = harveyData.map(key =>
     <li>{key.class + ", " + key.building + ", " + key.time}</li>
