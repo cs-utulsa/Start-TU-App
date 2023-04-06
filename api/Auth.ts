4cdbe97093a0e04772a0178e1bd50f50ae16cc80
Luke Runnels <lar9482@utulsa.edu>
2023-04-06 16:17:33 -0500

    Removed unnecessary code in Auth.ts

--- api/Auth.ts
+++ api/Auth.ts
@@ -81,29 +81,4 @@ export const retrieve_token = async (auth_code: string): Promise<string> => {
             reject(error.response.data.error_description)
         });
     })
-    const callback_url = 'msauth.com.example.StartTU://auth'
-
-    let url = 'https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token'
-
-    url = url.replace('{tenant}', Tenant_Id)
-
-    const requestBody = new URLSearchParams();
-    requestBody.append('grant_type', 'authorization_code');
-    requestBody.append('code', auth_code);
-    requestBody.append('client_id', Client_Id)
-    requestBody.append('redirect_uri', callback_url)
-
-    const config: AxiosRequestConfig = {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    };
-
-    axios.post(url, requestBody.toString(), config)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error(error.response.data.error_description);
-    });
 }
